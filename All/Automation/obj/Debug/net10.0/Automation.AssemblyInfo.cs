958895a34d7ad695e7e828dbe65c3c399b1ce99d
DavidIsle <DavidIsle@DavidIsle>
2025-11-16 12:39:36 +0000

    piplenes

--- All/Automation/obj/Debug/net10.0/Automation.AssemblyInfo.cs
+++ All/Automation/obj/Debug/net10.0/Automation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0151b4088affc4efa700b1acd0564c174926cb65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07092e74936bfee29d5a591f584e165bfabaab4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
